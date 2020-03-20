c497ca5ff10c43a22257ac0f2639c9731d570e01
paolabay <mpbayonalatorre@gmail.com>
2020-03-20 11:03:45 -0500

    cambio en los modelo
    cambio

--- hotelesbackend/hotel/models.py
+++ hotelesbackend/hotel/models.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
@@ -9,6 +8,8 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 import datetime
 from django.db import models
+
+
 # Create your models here.
 class Usuario(AbstractUser, PolymorphicModel):
     nombre = models.CharField(max_length=500)
@@ -22,8 +23,8 @@ class Usuario(AbstractUser, PolymorphicModel):
 
     objects = UserManager()
 
-class Hotel(Usuario):
 
+class Hotel(Usuario):
     class Meta:
         verbose_name = 'hotel'
         verbose_name_plural = 'hoteles'
@@ -31,8 +32,8 @@ class Hotel(Usuario):
     def __str__(self):
         return self.nombre
 
-class Cliente(Usuario):
 
+class Cliente(Usuario):
     class Meta:
         verbose_name = 'cliente'
         verbose_name_plural = 'clientes'
@@ -40,26 +41,41 @@ class Cliente(Usuario):
     def __str__(self):
         return self.nombre
 
+
+class Habitacion(models.Model):
+    nombre = models.CharField(max_length=100)
+    numeroCamas = models.IntegerField()
+    costo = models.IntegerField()
+    tipo = models.CharField(max_length=500)
+    pHotel = models.ForeignKey(Hotel, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tipo
+
+
 class Reserva(models.Model):
     fechaInicio = models.DateTimeField(default=datetime.datetime.utcnow)
     fechaFin = models.DateTimeField(default=datetime.datetime.utcnow)
     fechaReserva = models.DateTimeField(default=datetime.datetime.utcnow)
-    pHotel = models.ForeignKey(Hotel,  null=False, on_delete=models.CASCADE)
+    pHotel = models.ForeignKey(Hotel, null=False, on_delete=models.CASCADE)
     pCliente = models.ForeignKey(Cliente, null=False, on_delete=models.CASCADE)
+    pHabitacion = models.ManyToManyField( Habitacion)
     descripcion = models.CharField(max_length=500)
     pago = models.IntegerField()
-
 
     def __str__(self):
         return self.descripcion
 
-class Habitacion(models.Model):
-    numeroCamas = models.IntegerField()
-    costo =  models.IntegerField()
-    tipo = models.CharField(max_length=500)
-    pReserva = models.ForeignKey(Reserva, models.SET_NULL, blank=True, null=True )
-    pHotel = models.ForeignKey(Hotel, null=False, on_delete=models.CASCADE)
 
+
+
+class Servicio(models.Model):
+    fueCumplida = models.IntegerField()
+    descripcion = models.CharField(max_length=500)
+    pHotel = models.ForeignKey(Hotel, null=False, on_delete=models.CASCADE)
+    pReserva = models.ForeignKey(Reserva, null=False, on_delete=models.CASCADE)
+    pHabitacion = models.ForeignKey(Habitacion, null=False, on_delete=models.CASCADE)
+    pCliente = models.ForeignKey(Cliente, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.tipo
+        return self.descripcion
--- hotelesbackend/hotel/serializers.py
+++ hotelesbackend/hotel/serializers.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import *
+
+class HabitacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habitacion
+        fields = '__all__'
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['id', 'nombre', 'descripcion' ]
+
 class ReservaSerializer(serializers.ModelSerializer):
+    pHabitacion =  HabitacionSerializer(read_only=True, many=True)
+    pCliente = ClienteSerializer(read_only=True)
     class Meta:
         model = Reserva
         fields = '__all__'
@@ -11,12 +24,5 @@ class HotelSerializer(serializers.ModelSerializer):
         model = Hotel
         fields = '__all__'
 
-class ClienteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = '__all__'
 
-class HabitacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habitacion
-        fields = '__all__'
+
